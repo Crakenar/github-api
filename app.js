720be2e4d7e1be1aed13f8a532ce17f115cd3ad7
Crakenar <teothou@hotmail.fr>
2023-09-15 22:29:30 +0200

    Add read README.md files from github

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
-const express = require('express');
+const express = require("express");
+const { Octokit } = require("octokit");
+const fs = require("fs");
+const MarkdownIt = require("markdown-it");
+require("dotenv").config();
 
+const md = new MarkdownIt();
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (error) => {
-    if(!error) {
-        console.log(`Server is running on port ${PORT}`);
-    } else {
-        console.log('Error occured, server cannot start',error);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/githubReadme/:repoName", async (req, res) => {
+  const GITHUB_API_KEY = process.env.GITHUB_API_KEY;
+  const octokit = new Octokit({
+    auth: GITHUB_API_KEY,
+  });
+  const {
+    data: { login },
+  } = await octokit.rest.users.getAuthenticated();
+  if (!login) {
+    res.status(404).send("User not found");
+  }
+  const githubResponse = await octokit.request(
+    `GET /repos/{owner}/{repo}/contents/{path}`,
+    {
+      owner: login,
+      repo: req.params.repoName,
+      path: "README.md",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
     }
-})
+  );
+  if (githubResponse.status === 200) {
+    const buff = Buffer.from(githubResponse.data.content, "base64");
+    fs.writeFileSync("tmp.md", buff);
+    res.status(200).send(md.render(fs.readFileSync("tmp.md", "utf8")));
+  } else {
+    res.status(500).send(githubResponse.data.message);
+  }
+});
+
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log(`Server is running on port ${PORT}`);
+  } else {
+    console.log("Error occured, server cannot start", error);
+  }
+});
